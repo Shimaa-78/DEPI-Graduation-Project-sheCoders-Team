402d2ba02ea783b132972cb0ae49806f322d9c06
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2024-09-28 19:56:49 +0300

    upadting some changing in cart cubit and merging with nehal login and sign in

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: StartScreen(),
+          body: CartScreen(),
         ),
       ),
     );
