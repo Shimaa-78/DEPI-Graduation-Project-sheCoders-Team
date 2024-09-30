99ea298a3c60fb2960eaf4bdad4159c3deb86959
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2024-09-30 23:55:00 +0300

    Add to cart Complete

--- lib/cubit/cart_state.dart
+++ lib/cubit/cart_state.dart
@@ -4,18 +4,21 @@ part of 'cart_cubit.dart';
 sealed class CartState {}
 
 final class CartInitial extends CartState {}
+
 final class CartLoading extends CartState {}
 final class CartSuccess extends CartState {}
 final class CartError extends CartState {
   String? message;
   CartError( this.message);
 }
+
 final class addTocartLoading extends CartState {}
 final class addTocartSuccess extends CartState {}
 final class addTocartCartError extends CartState {
   String? message;
   addTocartCartError( this.message);
 }
+
 final class updateCartLoading extends CartState {}
 final class updateCartSuccess extends CartState {}
 final class updateCartError extends CartState {
