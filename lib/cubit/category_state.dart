8fb927193f3acdaa1bbf44310badcaf90d0921c1
ShahdMohammed224 <147397370+ShahdMohammed224@users.noreply.github.com>
2024-09-30 21:10:07 +0300

    categoryandproduct

---
+++ lib/cubit/category_state.dart
@@ -0,0 +1,19 @@
+part of 'category_cubit.dart';
+
+@immutable
+abstract class CategoryState {}
+
+class CategoryInitial extends CategoryState {}
+
+class CategoryLoading extends CategoryState {}
+
+class CategoryLoaded extends CategoryState {
+  final List<CategoryModel> categories;
+  CategoryLoaded({required this.categories});
+}
+
+class CategoryError extends CategoryState {
+  final String message;
+  CategoryError({required this.message});
+}
+
