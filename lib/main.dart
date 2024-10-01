9fca9b816f1a6d77c97fea026b0a460a9e29b77c
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2024-10-01 05:46:41 +0300

    Favourite Functionalities Complete

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: CategoryView(),
+          body: StartScreen(),
         ),
       ),
     );
