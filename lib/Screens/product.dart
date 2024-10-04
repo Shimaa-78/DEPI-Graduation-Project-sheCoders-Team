6fdda723c63b91e92f83baa8e9b3ad3cd65598d9
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2024-10-04 16:54:04 +0300

    Favourite Functionalities Complete

--- lib/Screens/product.dart
+++ lib/Screens/product.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
-import '../Widgets/productdetails.dart';
 import '../Models/ProducModel.dart';
 import '../Widgets/Row.dart';
-class Productveiw extends  StatelessWidget {
+import '../Widgets/productdetails.dart';
 
-   Product product;
-   Productveiw({required this.product});
+class Productveiw extends StatelessWidget {
+  final Product product;  // Non-nullable
+  Productveiw({required this.product});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          Productdetails(product: product),
 
-   body:  Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Productdetails(product: product),
-        SizedBox(height: 16),
-        Rowbar(product:product),
-        // إضافة مسافة بين النص والزر
-
-      ],
-    ));
+          Rowbar(product: product),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+ import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shoppe/Screens/startScreen.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart'; // Correct import for Hive Flutter initialization
 import 'package:shoppe/SCreens/categoriesview.dart';
@@ -9,6 +12,8 @@ import 'package:shoppe/Screens/startScreen.dart'; // Adjust casing to match actu
 import 'package:shoppe/cubit/favourite_cubit.dart';
 
 import 'Cubit/login_cubit.dart';
+import 'Cubit/personal_details_cubit.dart';
+import 'Cubit/profile_cubit.dart';
 
 import 'Screens/Cart.dart';
 import 'Screens/favorite.dart';
@@ -20,6 +25,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Moved to the right position
   await Hive.initFlutter();
   await Hive.openBox(HiveHelper.token);
+  await Hive.openBox('USER_BOX');
+  await Hive.openBox(HiveHelper.userPhoneNumber);
   DioHelper.inint();
 
   runApp(const MyApp());
@@ -42,12 +49,22 @@ class MyApp extends StatelessWidget {
           create: (context) => FavouriteCubit()..getFavouriteList(),
 
         ),
-      ],
+        BlocProvider(
+          create: (context) => PersonalDetailsCubit(),
+        ),
+        BlocProvider(
+          create: (context) => ProfileCubit(),
+          create: (context) => CartCubit()..getUserCart(),
+        ),
+        BlocProvider(
+          create: (context) => FavouriteCubit()..getFavouriteList(),
 
+        ),
+      ],
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: CategoryView(),
+          body:StartScreen(),
         ),
       ),
     );
